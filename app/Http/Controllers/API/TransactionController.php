77a33c3948d5845d548a3116e16d2156e229a526
muradha <mhdrizki098@gmail.com>
2024-01-16 01:10:50 +0700

    `add` : transactions crud api

---
+++ app/Http/Controllers/API/TransactionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\API\Transactions\StoreTransactionRequest;
+use App\Http\Requests\API\Transactions\UpdateTransactionRequest;
+use App\Http\Resources\TransactionCollection;
+use App\Http\Resources\TransactionResource;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $transactions = Transaction::all();
+
+        return new TransactionCollection($transactions);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTransactionRequest $request)
+    {
+        $data = $request->validated();
+
+        $transaction = Transaction::create($data);
+
+        return new TransactionResource($transaction);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Transaction $transaction)
+    {
+        return new TransactionResource($transaction);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTransactionRequest $request, Transaction $transaction)
+    {
+        $data = $request->validated();
+
+        $transaction->update($data);
+
+        return new TransactionResource($transaction);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Transaction $transaction)
+    {
+        $transaction->delete();
+
+        return new TransactionResource($transaction);
+    }
+}
---
+++ app/Http/Requests/API/Transactions/UpdateTransactionRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\API\Transactions;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTransactionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'period' => 'required|date',
+            'income' => 'required|numeric|max_digits:10',
+            'pay' => 'required|numeric|max_digits:10',
+            'account_id' => ['required', 'numeric', 'exists:accounts,id'],
+        ];
+    }
+}
