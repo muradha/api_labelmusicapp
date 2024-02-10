ed3e0dade35943e422fa2a5089048c7ce4b73e9d
muradha <mhdrizki098@gmail.com>
2024-02-10 15:39:47 +0700

    `add` : superadmin role

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
-use Spatie\Permission\Models\Role;
 
 /*
 |--------------------------------------------------------------------------
